8006c5da998a4be2efafe91fd6d2a731ab529efc
michelvaldrighi <michelvaldrighi>
2002-08-29 13:40:29 +0000

    use updated class.POP3.php instead of .php3

--- b2mail.php
+++ b2mail.php
@@ -5,7 +5,7 @@
 
 require('b2config.php');
 require($b2inc.'/b2vars.php');
-require($b2inc.'/class.POP3.php3');
+require($b2inc.'/class.POP3.php');
 require($b2inc.'/b2functions.php');
 require($b2inc."/b2template.functions.php");
 require($b2inc."/xmlrpc.inc");
