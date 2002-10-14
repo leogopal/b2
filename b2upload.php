051327d2cc23d3b0c96bb7806cadd55089cb76a7
michelvaldrighi <michelvaldrighi>
2002-10-14 23:43:41 +0000

    now asks confirmation for overwriting or using an alternate name for the file

--- b2upload.php
+++ b2upload.php
@@ -40,7 +40,7 @@ textarea,input,select {
 	background-color: #cccccc;
 	filter: alpha(opacity:80);
 <?php } ?>
-*/	border-width: 1px;
+*/  border-width: 1px;
 	border-color: #cccccc;
 	border-style: solid;
 	padding: 2px;
@@ -103,7 +103,10 @@ if (!$HTTP_POST_VARS["submit"]) {
 	<form action="b2upload.php" method="post" enctype="multipart/form-data">
 	<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $fileupload_maxk*1024 ?>" />
 	<input type="file" name="img1" size="30" class="uploadform" />
-	<br />
+	<br /><br />
+	Description:<br />
+	<input type="text" name="imgdesc" size="30" class="uploadform" />
+	<br /><br />
 	<input type="submit" name="submit" value="upload !" class="search" />
 	</form>
 	</td>
@@ -122,33 +125,94 @@ if (!$HTTP_POST_VARS["submit"]) {
 
 <?php  //Makes sure they choose a file
 
-if ($img1_name != "") {
+//print_r($HTTP_POST_FILES);
+//die();
+
+if (!empty($HTTP_POST_VARS)) { //$img1_name != "") {
+
+	$imgalt = (isset($HTTP_POST_VARS['imgalt'])) ? $HTTP_POST_VARS['imgalt'] : $imgalt;
+
+	$img1_name = (strlen($imgalt)) ? $HTTP_POST_VARS['imgalt'] : $HTTP_POST_FILES['img1']['name'];
+	$img1_type = (strlen($imgalt)) ? $HTTP_POST_VARS['img1_type'] : $HTTP_POST_FILES['img1']['type'];
+	$imgdesc = str_replace('"', '&amp;quot;', $HTTP_POST_VARS['imgdesc']);
 
 	$imgtype = explode(".",$img1_name);
 	$imgtype = " ".$imgtype[count($imgtype)-1]." ";
 
 	if (!ereg(strtolower($imgtype), strtolower($fileupload_allowedtypes))) {
-		die("This file type $imgtype is not allowed by the admin of this blog.");
+	    die("File $img1_name of type $imgtype is not allowed.");
 	}
-	$pathtofile = $fileupload_realpath."/".$img1_name;
+
+	if (strlen($imgalt)) {
+		$pathtofile = $fileupload_realpath."/".$imgalt;
+		$img1 = $HTTP_POST_VARS['img1'];
+	} else {
+		$pathtofile = $fileupload_realpath."/".$img1_name;
+		$img1 = $HTTP_POST_FILES['img1']['tmp_name'];
+	}
 
 	// makes sure not to upload duplicates, rename duplicates
 	$i = 1;
-	$tmppathtofile = $pathtofile;
-	while (file_exists($pathtofile)) {
-		$pos = strpos($tmppathtofile, '.'.trim($imgtype));
-		$pathtofile_start = substr($tmppathtofile, 0, $pos);
-		$pathtofile = $pathtofile_start.'_'.zeroise($i++, 2).'.'.trim($imgtype);
-		$img1_name = explode('/', $pathtofile);
-		$img1_name = $img1_name[count($img1_name)-1];
+	$pathtofile2 = $pathtofile;
+	$tmppathtofile = $pathtofile2;
+	$img2_name = $img1_name;
+
+	while (file_exists($pathtofile2)) {
+	    $pos = strpos($tmppathtofile, '.'.trim($imgtype));
+	    $pathtofile_start = substr($tmppathtofile, 0, $pos);
+	    $pathtofile2 = $pathtofile_start.'_'.zeroise($i++, 2).'.'.trim($imgtype);
+	    $img2_name = explode('/', $pathtofile2);
+	    $img2_name = $img2_name[count($img2_name)-1];
 	}
 
-	move_uploaded_file("$img1" , "$pathtofile") //Path to your images directory, chmod the dir to 777
-	or die("Couldn't Upload Your File to $pathtofile.");
+	if (file_exists($pathtofile) && !strlen($imgalt)) {
+		$i = explode(" ",$fileupload_allowedtypes);
+		$i = implode(", ",array_slice($i, 1, count($i)-2));
+		move_uploaded_file($img1, $pathtofile2) 
+		 or die("Couldn't Upload Your File to $pathtofile2.");
+	
+	// duplicate-renaming function contributed by Gary Lawrence Murphy
+	?>
+	<p><strong>Duplicate File?</strong></p>
+	<p><b><em>The filename '<?php echo $img1_name; ?>' already exists!</em></b></p>
+	<p> filename '<?php echo $img1; ?>' moved to '<?php echo "$pathtofile2 - $img2_name"; ?>'</p>
+	<p>Confirm or rename:</p>
+	<form action="b2upload.php" method="post" enctype="multipart/form-data">
+	<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $fileupload_maxk*1024 ?>" />
+	<input type="hidden" name="img1_type" value="<?php echo $img1_type;?>" />
+	<input type="hidden" name="img1_name" value="<?php echo $img2_name;?>" />
+	<input type="hidden" name="img1" value="<?php echo $pathtofile2;?>" />
+	Alternate name:<br /><input type="text" name="imgalt" size="30" class="uploadform" value="<?php echo $img2_name;?>" /><br />
+	<br />
+	Description:<br /><input type="text" name="imgdesc" size="30" class="uploadform" value="<?php echo $imgdesc;?>" />
+	<br />
+	<input type="submit" name="submit" value="confirm !" class="search" />
+	</form>
+	</td>
+	</tr>
+	</tbody>
+</table>
+</body>
+</html><?php die();
+
+	}
+
+	if (!strlen($imgalt)) {
+		move_uploaded_file($img1, $pathtofile) //Path to your images directory, chmod the dir to 777
+		 or die("Couldn't Upload Your File to $pathtofile.");
+	} else {
+		rename($img1, $pathtofile)
+		or die("Couldn't Upload Your File to $pathtofile.");
+	}
 
 }
 
-$piece_of_code = "&lt;img src=&quot;$fileupload_url/$img1_name&quot; border=&quot;0&quot; alt=&quot;&quot; /&gt;";
+
+if ( ereg('image/',$img1_type)) {
+	$piece_of_code = "&lt;img src=&quot;$fileupload_url/$img1_name&quot; border=&quot;0&quot; alt=&quot;$imgdesc&quot; /&gt;"; 
+} else {
+	$piece_of_code = "&lt;a href=&quot;$fileupload_url/$img1_name&quot; title=&quot;$imgdesc&quot; /&gt;$imgdesc&lt;/a&gt;"; 
+};
 
 ?>
 
