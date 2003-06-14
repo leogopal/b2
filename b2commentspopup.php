8b5bc36c6dc8dc3cb8bb10e323e3e4c7dc3cdef3
mikelittle <mikelittle>
2003-06-14 23:04:10 +0000

    Removed spurious highbyte character.

--- b2commentspopup.php
+++ b2commentspopup.php
@@ -97,7 +97,7 @@ while($rowc = mysql_fetch_object($resultc)) {
 	<p class="commentfield">
 	your comment<br />
 	<textarea cols="40" rows="4" name="comment" tabindex="4" class="textarea"
-Ê   onfocus="this.value=(this.value=='comment') ? '' : this.value;" onblur="this.value=(this.value=='') ? 'comment' : this.value;" >comment</textarea> 
+    onfocus="this.value=(this.value=='comment') ? '' : this.value;" onblur="this.value=(this.value=='') ? 'comment' : this.value;" >comment</textarea>
 	</p>
 
 	<p class="commentfield">
